d6c0cb6033f01b3d2f8140a86972d68a6b0daca0
Horacio Rodrigo Morales Cruz <a2018119192@estudiantes.upsa.edu.bo>
2021-01-24 22:58:56 -0400

    Con modelo y controlador

---
+++ SolucionAuto/WebAuto/Models/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAuto.Models
+{
+    public enum VehicleType
+    { 
+        Vagoneta=0,
+        Sedan=1,
+        Jeep=2,
+        Camioneta=3
+    }
+    public enum WheelDrive
+    {
+        AWD = 0,
+        TWOWD = 1,      
+    }
+    public class Car
+    {
+        [Key]
+        public int CarId { get; set; }
+        [Required]
+        [StringLength(20,MinimumLength =3)]
+        public string Brand { get; set; }
+        public VehicleType Vehicle { get; set; }    
+        [Range(1000,100000)]
+        public double Price { get; set; }
+        public WheelDrive WheelD { get; set; }
+
+    }
+}
---
+++ SolucionAuto/WebAuto/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAuto.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(): base ("DefaultConnection")
+        {
+                
+        }
+
+        public System.Data.Entity.DbSet<WebAuto.Models.Car> Cars { get; set; }
+    }
+}
